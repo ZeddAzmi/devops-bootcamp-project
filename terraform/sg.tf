62e6a543b131eaea2a0d0bc980aceb7d7b7e629e
DinAzmi <zeddinn311@gmail.com>
2026-01-02 02:25:09 +0800

    reconfigure terrform security group file

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -22,33 +22,3 @@ resource "aws_security_group" "private" {
     Name = "devops-private-sg"
   }
 }
-
-resource "aws_security_group" "public" {
-  name   = "devops-public-sg"
-  vpc_id = aws_vpc.this.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "devops-public-sg"
-  }
-}
