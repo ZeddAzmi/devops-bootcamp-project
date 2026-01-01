b085bc78d3944f91adc4f6a0cd9ed46abb32f11b
DinAzmi <zeddinn311@gmail.com>
2026-01-02 04:13:53 +0800

    Remove SSM user_data from web server after troubleshot web server unable access via ssm

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,14 +1,19 @@
 resource "aws_instance" "web" {
-  ami                    = "ami-00d8fc944fb171e29"
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.public.id
-  private_ip             = "10.0.0.5"
+  ami           = "ami-00d8fc944fb171e29"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public.id
+  private_ip    = "10.0.0.5"
+
   vpc_security_group_ids = [aws_security_group.public.id]
-  tags                   = { Name = "web-server" }
+
+  tags = {
+    Name = "web-server"
+  }
 }
 
+
 resource "aws_instance" "ansible" {
-  ami                    = "ami-00d8fc944fb171e29"
+  ami           = "ami-00d8fc944fb171e29"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.private.id
   private_ip    = "10.0.0.135"
