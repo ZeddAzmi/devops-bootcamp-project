1b93beeb3669f8be3dd35ae8cbb3ca23032cc4a7
DinAzmi <zeddinn311@gmail.com>
2026-01-10 11:43:42 +0800

    Add Internet Gateway and public route table

---
+++ terraform/routes.tf
@@ -0,0 +1,19 @@
+# Public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "devops-public-rt"
+  }
+}
+
+# Associate public subnet with public route table
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
