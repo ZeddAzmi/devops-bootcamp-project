b23831b3c257efd1af19e25288a915d814c4df3d
DinAzmi <zeddinn311@gmail.com>
2026-01-10 12:06:24 +0800

    Add public and private security groups

---
+++ terraform/security-groups.tf
@@ -0,0 +1,79 @@
+#################################
+# Public Security Group (Web)
+#################################
+resource "aws_security_group" "public_sg" {
+  name        = "devops-public-sg"
+  description = "Public SG for web server"
+  vpc_id      = aws_vpc.main.id
+
+  # SSH (limit to VPC for now)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  # HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ICMP within VPC
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-public-sg"
+  }
+}
+
+#################################
+# Private Security Group
+#################################
+resource "aws_security_group" "private_sg" {
+  name        = "devops-private-sg"
+  description = "Private SG for ansible & monitoring"
+  vpc_id      = aws_vpc.main.id
+
+  # SSH only from inside VPC
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  # ICMP within VPC
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-private-sg"
+  }
+}
